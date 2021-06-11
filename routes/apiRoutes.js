be193704a1e7e934a3103b23822fccdb79d73b80
Burke <beatty.burke@gmail.com>
2021-06-11 13:35:18 -0400

    app.post function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,25 @@
 const fs = require('fs')
-const data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+const data = JSON.parse(fs.readFileSync("../db/db.json", "utf-8"))
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(data));
 
-    app.post('/api/notes/:id', (req, res) => {
+    app.get('/api/notes/:id', (req, res) => {
         res.json(data[Number(req.params.id)])
-
     })
+
+    app.post('/api/notes', (req, res) => {
+        let note = req.body;
+        let uniqueID = (data.length)
+        console.log(uniqueID);
+        note.id = uniqueID;
+        data.push(note);
+
+        fs.writeFileSync("../db/db.json", JSON.stringify(data), (err) => { 
+            if (err) throw (err)
+        })
+
+        res.json(data)
+    }); 
 }
 
