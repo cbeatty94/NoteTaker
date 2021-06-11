2e99993b5e508b3e562354295fae5b97f1517a67
Burke <beatty.burke@gmail.com>
2021-06-11 13:02:39 -0400

    module.exports

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,7 @@
 const path = require('path');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+module.exports = (app) => {
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/assets/index.html')));
+    
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/assets/notes.html')));
+}
